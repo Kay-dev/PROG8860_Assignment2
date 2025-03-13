e72bcbe33629685bfe648c3ea4e518659f54255c
zhengwei <a932327192@gmail.com>
2025-03-13 18:26:33 -0400

    Initial commit

---
+++ test/cdk-project-8960392.test.js
@@ -0,0 +1,17 @@
+// const cdk = require('aws-cdk-lib');
+// const { Template } = require('aws-cdk-lib/assertions');
+// const CdkProject8960392 = require('../lib/cdk-project-8960392-stack');
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/cdk-project-8960392-stack.js
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//   // WHEN
+//   const stack = new CdkProject8960392.CdkProject8960392Stack(app, 'MyTestStack');
+//   // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
